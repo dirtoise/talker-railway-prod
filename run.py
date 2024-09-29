c6b209e5db9f23b5253a2c74c77e0cea052bf26e
dirtoise <mcsestoso@up.edu.ph>
2024-09-29 21:24:12 +0800

    talker(backend) w/build v1.0.0.13

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app, socketio
 from config import DevConfig, ProdConfig
+import os
 
 if __name__ == '__main__':
     app = create_app(ProdConfig)
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=os.getenv("PORT", default=5000))
